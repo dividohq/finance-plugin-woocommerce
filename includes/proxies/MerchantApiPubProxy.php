c8ec6eb442850a26dcfb298605c2131d1c1bb3ee
Freddie Line <freddieline@hotmail.com>
2024-04-15 17:05:14 +0100

    fix: use version not tenant id

--- includes/proxies/MerchantApiPubProxy.php
+++ includes/proxies/MerchantApiPubProxy.php
@@ -54,11 +54,10 @@ class MerchantApiPubProxy{
     const HEADER_KEYS = [
         'API_KEY' => 'X-DIVIDO-API-KEY',
         'SHARED_SECRET' => 'X-Divido-Hmac-Sha256',
-        'TENANT_ID' => 'X-DIVIDO-TENANT-ID',
+        'VERSION' => 'X-DIVIDO-VERSION',
     ];
 
-    // set tenant ID to be Divido as default
-    const TENANT_ID = 'divido'
+    const VERSION = '2'
 
     private HttpApiWrapper $wrapper;
 
@@ -67,7 +66,7 @@ class MerchantApiPubProxy{
         $this->wrapper->setHeader('Accept', 'application/json');
         $this->wrapper->setHeader('Content-Type', 'application/json');
         $this->wrapper->setHeader(self::HEADER_KEYS['API_KEY'], $apiKey);
-         $this->wrapper->setHeader(self::HEADER_KEYS['TENANT_ID'], TENANT_ID);
+         $this->wrapper->setHeader(self::HEADER_KEYS['VERSION'], VERSION);
     }
 
     /**
